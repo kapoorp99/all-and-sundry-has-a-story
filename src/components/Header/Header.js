4890c66fe182dad31353f7c5ea8e7d81fa6e01f1
Prakhar Kapoor <kapoorprakhar99@gmail.com>
2021-08-21 01:08:43 +0530

    header component implemented 🤗

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import { HeaderContainer } from './styles';
+import useWindowDimensions from '../../customHooks/useWindowDimensions'
 
-const Header = ({ title }) => {
+const Header = ({ navLink_title1, navLink_title2, navLink_title3, bgColor, textColor }) => {
+  const { width } = useWindowDimensions()
   return (
-    <HeaderContainer>
-      { title }
+    <HeaderContainer bgColor={bgColor} textColor={textColor}>
+      <div className='left'>
+        <div className='header__logo__container'>
+          <img className='header__logo' src='https://img.icons8.com/ios-filled/50/000000/centralized-network.png' alt='' />
+        </div>
+      </div>
+      <div className='right'>
+        {width < 768 ? (
+          <div className='header__right__mobile'>
+            <img className='header__logo' src='https://img.icons8.com/ios/50/000000/menu-squared-2--v1.png' alt='' />
+          </div>
+        ) : (
+          <>
+            <div>
+              <h4 className='header__nav__links'>{navLink_title1}</h4>
+              <h4 className='header__nav__links'>{navLink_title2}</h4>
+              <h4 className='header__nav__links'>{navLink_title3}</h4>
+            </div>
+            <div>
+              <button className='header__btn login'>Login</button>
+              <button className='header__btn signup'>Sign up</button>
+            </div>
+          </>
+        )}
+      </div>
     </HeaderContainer>
   )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,7 +1,74 @@
 import styled from "styled-components"
 
 export const HeaderContainer = styled.div`
-    background-color: #171717;
-    color: #ffffff;
-    text-align: center;
+    background-color: ${(props => props.bgColor)};
+    color: ${(props) => props.textColor};
+    display: flex;
+    align-items: center;
+    padding: 0.1rem;
+    width: 100%;
+    >.left {
+        margin: 0.8rem 0.5rem;
+    }
+    >.right {
+        margin: 0.8rem 0.5rem;
+        display: flex;
+        flex-grow: 1;
+        >div:nth-child(1) {
+            display: flex;
+            flex-grow: 5;
+            justify-content: center;
+            align-items: center;
+        }
+        >div:nth-child(2) {
+            display: flex;
+            flex-grow: 1;
+            justify-content: flex-end;
+        }
+    }
+    .header__logo__container {
+        height: 100%;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .header__logo {
+        max-width: 100%;
+        height: auto;
+        cursor: pointer;
+    }
+    .header__nav__links {
+        cursor: pointer;
+        font-size: larger;
+        font-weight: 600;
+        margin: 0 0.8rem;
+    }
+    .header__btn {
+        outline: none;
+        border-radius: 0.6rem;
+        width: 5rem;
+        margin: 0 0.5rem;
+        padding: 0.5rem;
+        cursor: pointer;
+        font-weight: 500;
+    }
+    .login {
+        border: 1px solid #000000;
+        background-color: transparent;
+    }
+    .signup {
+        border: none;
+        background-color: #000000;
+        color: #ffffff;
+    }
+    @media(max-width: 768px) {
+        .right {
+            >div:nth-child(1) {
+                display: flex;
+                justify-content: flex-end;
+                padding: 0 0.5rem;
+            }
+        }
+    }
 `;
